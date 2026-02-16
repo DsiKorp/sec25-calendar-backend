481fe4f50809191388fa89962ff3deaba4c23da9
Juan Delgado <dsikorp@gmail.com>
2026-02-15 21:24:23 -0500

    Events Routes with validarJwt

--- src/routes/eventsRoutes.ts
+++ src/routes/eventsRoutes.ts
@@ -1,12 +1,18 @@
+/*
+    Rutas de eventos /events
+    host + /api/events
+*/
+
 import { Router } from 'express';
+import { validarJWT } from '../middlewares/validar-jwt';
 import { actualizarEvento, crearEvento, eliminarEvento, getEventos } from '../controllers/eventsController';
 
 const router = Router();
 
-router.get('/', getEventos);
-router.post('/', crearEvento);
-router.put('/:id', actualizarEvento);
-router.delete('/:id', eliminarEvento);
+router.get('/', validarJWT, getEventos);
+router.post('/', validarJWT, crearEvento);
+router.put('/:id', validarJWT, actualizarEvento);
+router.delete('/:id', validarJWT, eliminarEvento);
 
 
 module.exports = router;
